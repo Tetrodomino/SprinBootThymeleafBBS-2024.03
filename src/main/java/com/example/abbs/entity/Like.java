5178aba215e0d800a30e790305d33f1cc7f798cc
Tetrodomino <wettrimotto@naver.com>
2024-03-08 16:19:25 +0900

    2024-03-08

--- src/main/java/com/example/abbs/entity/Like.java
+++ src/main/java/com/example/abbs/entity/Like.java
@@ -1,10 +1,82 @@
 package com.example.abbs.entity;
 
+import java.time.LocalDateTime;
+
 public class Like {
 
 	private int lid;
 	private String uid;
 	private int bid;
 	private int value;
+	private LocalDateTime clickTime;
+	
+	public Like() {
+	}
+
+	public Like(String uid, int bid) {
+		this.uid = uid;
+		this.bid = bid;
+	}
+
+	public Like(int lid, String uid, int bid, int value) {
+		this.lid = lid;
+		this.uid = uid;
+		this.bid = bid;
+		this.value = value;
+	}
+
+	public Like(int lid, String uid, int bid, int value, LocalDateTime clickTime) {
+		this.lid = lid;
+		this.uid = uid;
+		this.bid = bid;
+		this.value = value;
+		this.clickTime = clickTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Like [lid=" + lid + ", uid=" + uid + ", bid=" + bid + ", value=" + value + ", clickTime=" + clickTime
+				+ "]";
+	}
+
+	public int getLid() {
+		return lid;
+	}
+
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public int getBid() {
+		return bid;
+	}
+
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public LocalDateTime getClickTime() {
+		return clickTime;
+	}
+
+	public void setClickTime(LocalDateTime clickTime) {
+		this.clickTime = clickTime;
+	}
 	
 }
---
+++ src/main/resources/static/js/aside.js
@@ -0,0 +1,28 @@
+/*
+    Aside menu control
+*/
+// 이벤트 등록
+$(document).ready(function() {
+    $('#stateMsgBtn').click(function(e) {
+        $('#stateMsgInput').attr({'class': 'mt-2'}); // 입력창 보이게 하기
+        $('#stateInput').val($('#stateMsg').text()); // 입력창에 id값이 stateMsg인 태그의 글을 넣음
+    });
+    $('#stateMsgSubmit').click(changeStateMsg); // 이벤트 등록
+});
+
+function changeStateMsg() {
+    let stateInputVal = $('stateInput').val(); // 사용자가 수정한 글 읽기
+    $('#stateMsgInput').attr({'class': 'mt-2 d-none'}); // 입력창 감추기
+    
+    // 입력시킨 내용을 보내기
+    // Asynchronous Javascript and XML, 화면의 일부분만 바꿀 때 주로 사용
+    $.ajax({
+        type: 'GET', 
+        url: '/abbs/aside/stateMsg',
+        data: {stateMsg: stateInputVal},
+        success: function(e) {
+            console.log('state message:', stateInputVal, e);
+            $('#stateMsg').html(stateInputVal);
+        }
+    });
+}
